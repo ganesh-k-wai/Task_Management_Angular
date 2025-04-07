334e042520e9840ad69f4676caaf978e7f1149b6
ganesh-k-wai <ganesh.kadam@waiin.com>
2025-04-07 19:35:48 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProjectComponentComponent } from './project-component/project-component.component';
 import { TaskComponentComponent } from './task-component/task-component.component';
-import { TaskListComponentComponent } from './task-list-component/task-list-component.component';
+// import { TaskListComponentComponent } from './task-list-component/task-list-component.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'project-comp', component: ProjectComponentComponent },
   { path: 'task-comp/:project_Id', component: TaskComponentComponent },
-  { path: 'task-list', component: TaskListComponentComponent },
+  // { path: 'task-list', component: TaskListComponentComponent },
 ];
